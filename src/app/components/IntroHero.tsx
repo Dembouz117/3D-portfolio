884002ec3f5326b8b9c3f07341b35955fc4770b2
Dembouz117 <dembouz117@gmail.com>
2024-01-06 18:42:16 +0800

    added descriptive sections

--- src/app/components/IntroHero.tsx
+++ src/app/components/IntroHero.tsx
@@ -4,7 +4,7 @@ import styles from "@/app/styles/IntroHero.module.css";
 
 const IntoHero = () => {
   return (
-    <div className="bg-gradient-to-b from-red-100 to-red-500 p-8 relative text-white flex flex-col items-center w-10/12 h-72">
+    <div className="bg-gradient-to-b from-red-100 to-red-600 p-8 relative text-white flex flex-col items-center w-10/12 h-72 rounded-lg">
       <div className={`font-bungee font-extrabold text-7xl mt-12`}>
         HELLO, I'M FADHEL
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 import Image from 'next/image';
 import FirstMesh from './components/FirstMesh';
-import IntroHero from "@/app/components/IntroHero"
+import IntroHero from "@/app/components/IntroHero";
+import About from "@/app/components/About";
 export default function Home() {
   return (
-    <main className="flex flex-col items-center pt-16 relative bg-blue-500">
+    <main className="flex flex-col items-center pt-16 relative">
       {/* <FirstMesh/> */}
       <IntroHero/>
+      <About/>
     </main>
   )
 }
